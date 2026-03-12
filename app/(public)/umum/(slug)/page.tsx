41f3e297e18a4853c53cdc2bf6364208629fb659
rsydhazh <rosyknj5297@gmail.com>
2026-03-12 11:03:11 +0700

    fix: menambahkan export default di halaman slug umum

---
+++ app/(public)/umum/(slug)/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function DetailUmumPage() {
+  return (
+    <div>
+      <h1>Halaman Detail Umum</h1>
+      <p>Sedang dalam pengembangan...</p>
+    </div>
+  )
+}
