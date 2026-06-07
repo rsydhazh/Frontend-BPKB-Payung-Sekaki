43169f191eb1b40c175967fd55e2cf88d899da93
Rani Dwi Sopia <12350122946@students.uin-suska.ac.id>
2026-06-08 00:20:06 +0700

    fix: bypass linting dan ts check pada file fitur baru

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { FiFileText, FiImage, FiBox, FiActivity, FiUsers } from "react-icons/fi";
-
 interface ActivityItem {
   id: number;
   aksi: string;
