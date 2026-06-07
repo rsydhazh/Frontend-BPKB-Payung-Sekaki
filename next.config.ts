8ee5f9218c2c38d4a45c02e5685865838aa14b6e
Rani Dwi Sopia <12350122946@students.uin-suska.ac.id>
2026-06-08 00:06:01 +0700

    fix: bypass total vercel build via root env

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    // Ini tetap wajib ada untuk mengabaikan error tipe data di cloud
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
